9a85b614e3ea8390f3da878f50fe8d61d3ce95e0
Changyeon-Seo <scy7351@naver.com>
2021-07-05 17:02:45 +0900

    exoplayer 라이브러리 추가, 유튜브와 유사한 영상 재생 화면 레이아웃 추가

--- app/src/main/java/com/mercy/kids/client/launch/LaunchActivity.kt
+++ app/src/main/java/com/mercy/kids/client/launch/LaunchActivity.kt
@@ -3,6 +3,7 @@ package com.mercy.kids.client.launch
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import com.mercy.kids.client.account.activity.LoginHomeActivity
+import com.mercy.kids.client.main.activity.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,11 +11,16 @@ class LaunchActivity: AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        navigateToAccountService()
+        navigateToMainActivity()
     }
 
     private fun navigateToAccountService() {
         startActivity(Intent(this, LoginHomeActivity::class.java))
+        finish()
+    }
+
+    private fun navigateToMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
 
